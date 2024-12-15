0a542bebf0e2528d06f8323b22e65264f0a81883
lobsterXC <a1115514@mail.nuk.edu.tw>
2024-12-16 00:20:27 +0800

    目前進度

--- student.php
+++ student.php
@@ -1,7 +1,26 @@
-<h2>學生系統</h2>
-<p>請選擇功能：</p>
-<ul>
-    <li><a href="student_login.php">學生登入</a></li>
-    <li><a href="student_register.php">報名系統</a></li>
-    <li><a href="student_history.php">歷屆作品瀏覽</a></li>
-</ul>
+<!DOCTYPE html>
+<html lang="zh-TW">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>高雄大學創意競賽管理系統</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <div class="navbar">
+            <img src="images/logo.png" alt="Logo" class="logo">
+            <h1>高雄大學激發學生創意競賽管理系統</h1>
+            </div>
+        </header>
+        <h2 style="text-align: center;" >學生系統</h2>
+        <!-- <p>請選擇功能：</p> -->
+        <div class="rounded-box">
+            <ul>
+                <li><a href="student_login.php">學生登入</a></li>
+                <li><a href="student_register.php">報名系統</a></li>
+                <li><a href="student_history.php">歷屆作品瀏覽</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
