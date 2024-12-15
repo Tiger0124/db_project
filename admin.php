57b670eed47f3e9e64fda9f247f839ebdc5426a7
lobsterXC <a1115514@mail.nuk.edu.tw>
2024-12-15 23:54:25 +0800

    <center>             <h2>管理員登入系統 </h2>         </center>

--- admin.php
+++ admin.php
@@ -1,8 +1,26 @@
-<h2>管理員登入系統</h2>
-<form action="admin_dashboard.php" method="POST">
-    <label for="username">用戶名</label>
-    <input type="text" id="username" name="username" required>
-    <label for="password">密碼</label>
-    <input type="password" id="password" name="password" required>
-    <button type="submit">登入</button>
-</form>
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>高雄大學創意競賽管理系統</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+        <img src="images/logo.png" alt="Logo" class="logo">
+        <h1>高雄大學激發學生創意競賽管理系統</h1>
+        </div>
+    </header>
+        <center> 
+            <h2>管理員登入系統 </h2> 
+        </center>
+    <form action="admin_dashboard.php" method="POST">
+        <label for="username">用戶名</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">密碼</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">登入</button>
+    </form>
+</body>
