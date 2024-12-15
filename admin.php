158cfd4b76d03a7d5b04fc5e8479fd860f218a51
lobsterXC <a1115514@mail.nuk.edu.tw>
2024-12-15 23:57:20 +0800

    正常的置中

--- admin.php
+++ admin.php
@@ -13,9 +13,9 @@
         <h1>高雄大學激發學生創意競賽管理系統</h1>
         </div>
     </header>
-        <center> 
-            <h2>管理員登入系統 </h2> 
-        </center>
+        
+        <h2 style="text-align: center;">管理員登入系統 </h2> 
+        
     <form action="admin_dashboard.php" method="POST">
         <label for="username">用戶名</label>
         <input type="text" id="username" name="username" required>
