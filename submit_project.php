557234687dd677dfd5041518cfeb336c7380f14e
lobsterXC <a1115514@mail.nuk.edu.tw>
2025-01-06 06:05:57 +0800

    小改

--- submit_project.php
+++ submit_project.php
@@ -1,4 +1,20 @@
-<link rel="stylesheet" href="styles.css">
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>高雄大學創意競賽管理系統</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+    <div class="navbar">
+        <a href="main.php" alt="Logo" class="logo">
+                <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+        <h1>高雄大學激發學生創意競賽管理系統</h1>
+    </div>
+    </header>
 <?php
 include 'conn.php';
 #接收表單的文字內容
@@ -60,3 +76,16 @@ echo "<input type='hidden' name='password' value='".$password."'>";
 echo "<button type='submit'>返回</button>";
 echo "</form>";
 ?>
+<footer class="site-footer">
+    <div class="footer-content">
+        <p>&copy; Copyright © 2025 XC Lee Tiger Lin  How Ho. All rights reserved.</p>
+        <div class="footer-row">
+        <div class="footer-container">
+            <p>聯絡我們 : <a href="mailto:wylin@nuk.edu.tw">wylin@nuk.edu.tw</a></p>
+        </div>
+        <ul class="footer-links">
+            <li><a href="https://github.com/Tiger0124/db_project.git">關於我們</a></li>
+        </ul>
+        </div>
+    </div>
+</footer>
