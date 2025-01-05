02b46ee1596feb6bc6a1b1a40dc1b2ce4d71b6b8
Tiger0124 <dogs247841@gmail.com>
2025-01-06 00:46:18 +0800

    1

--- judgement.php
+++ judgement.php
@@ -23,6 +23,7 @@
                     <thead>
                         <tr>
                             <th>隊伍名稱</th>
+                            <th>作品名稱</th>
                             <th>作品說明書</th>
                             <th>海報</th>
                             <th>影片網址</th>
@@ -32,39 +33,17 @@
                     </thead>
                     <tbody>
                     <form action="submit_score.php" method="POST">
-                    
-                        <!-- 隊伍 1 -->
-                        <!-- <tr>
-                            <td>創新未來隊</td>
-                            <td><a href="files/description1.pdf" download>下載說明書</a></td>
-                            <td><a href="files/poster1.pdf" download>下載海報</a></td>
-                            <td><a href="https://youtu.be/example1" target="_blank">影片連結</a></td>
-                            <td><a href="https://github.com/example/project1" target="_blank">程式碼連結</a></td>
-                            <td>
-                                <input type="number" id="score1" name="score1" min="1" max="100" required>
-                            </td>
-                        </tr> -->
-                        <!-- 隊伍 2 -->
-                        <!-- <tr>
-                            <td>未來之星隊</td>
-                            <td><a href="files/description2.pdf" download>下載說明書</a></td>
-                            <td><a href="files/poster2.pdf" download>下載海報</a></td>
-                            <td><a href="https://youtu.be/example2" target="_blank">影片連結</a></td>
-                            <td><a href="https://github.com/example/project2" target="_blank">程式碼連結</a></td>
-                            <td>
-                            <input type="number" id="score1" name="score1" min="1" max="100" required>    
-                            </td>
-                        </tr> -->
                     <?php
                         include 'conn.php';
                         // 獲取當前年份
                         $currentYear = date("Y");
                         $select_db = @mysqli_select_db($link, "db_project"); //選擇資料庫
-                        $sql = "SELECT 隊伍.隊伍編號, 隊伍.隊伍名稱, 作品.說明書, 作品.海報, 作品.作品展示_youtube連結, 作品.程式碼_Github連結 FROM 隊伍 natural join 作品 WHERE 隊伍.參加年份 = '$currentYear'";
+                        $sql = "SELECT 隊伍.隊伍編號, 隊伍.隊伍名稱, 作品.作品名稱, 作品.說明書, 作品.海報, 作品.作品展示_youtube連結, 作品.程式碼_Github連結 FROM 隊伍 natural join 作品 WHERE 隊伍.參加年份 = '$currentYear'";
                         $result = mysqli_query($link, $sql);
                         while($row = mysqli_fetch_assoc($result)){
                             echo "<tr>";
                             echo "<td>".$row['隊伍名稱']."</td>";
+                            echo "<td>".$row['作品名稱']."</td>";
                             $blob = base64_encode($row['說明書']);
                             echo "<td><a href='data:application/pdf;base64,".$blob."' download>下載說明書</a></td>";
                             $blob = base64_encode($row['海報']);
@@ -72,7 +51,6 @@
                             echo "<td><a href='".$row['作品展示_youtube連結']."' target='_blank'>影片連結</a></td>";
                             echo "<td><a href='".$row['程式碼_Github連結']."' target='_blank'>程式碼連結</a></td>";
                             echo "<td>";
-                            echo "<form action='submit_score.php' method='POST'>";
                             echo "<input type='number' id='score".$row['隊伍編號']."' name='score".$row['隊伍編號']."' min='1' max='100' required>";
                             echo "</td>";
                             echo "</tr>";
@@ -84,6 +62,21 @@
                 </table>
                 <div style="text-align: right;">
                     <button type="submit">提交評分</button>
+                    <!-- 年分 -->
+                    <!-- 編號 -->
+                    <!-- 評審身分證 -->
+                    <!-- 屆數 -->
+                     <?php 
+                     include 'conn.php';
+                     // 獲取當前年份
+                     $currentYear = date("Y");
+                     $select_db = @mysqli_select_db($link, "db_project"); //選擇資料庫
+                     $sql = "SELECT 隊伍.參加年份, 隊伍.隊伍編號, 評審委員.身分證字號, 評審委員.屆數 FROM 隊伍 natural join 評審委員 WHERE 隊伍.參加年份 = '$currentYear'";
+                        $result = mysqli_query($link, $sql);
+                        while($row = mysqli_fetch_assoc($result)){
+                            $sql1= "INSERT INTO 評分資料 (評分，參加年份, 隊伍編號, 身分證字號, 屆數) VALUES (score,'"$row['參加年份']"', '".$row['隊伍編號']."', '".$row['身分證字號']."', '".$row['屆數']."')";
+                        }
+                    ?>
                 </div>
 
             </form>
--- submit_score.php
+++ submit_score.php
@@ -1,34 +1,3 @@
 <?php
-// 連接資料庫
-include 'conn.php';
-
-// 檢查是否有提交表單
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // 遍歷提交的所有數據
-    foreach ($_POST as $key => $value) {
-        // 判斷是否為評分欄位
-        if (strpos($key, 'score') === 0) {
-            // 擷取隊伍編號
-            $teamId = str_replace('score', '', $key);
-            $score = intval($value); // 確保評分為整數
-
-            // 插入或更新評分資料表
-            $sql = "INSERT INTO 評分資料 (評分, 隊伍編號) 
-                    VALUES ('$score', '$teamId')
-                    ON DUPLICATE KEY UPDATE 評分 = '$score'";
-
-            // 執行 SQL 語句
-            if (mysqli_query($link, $sql)) {
-                echo "評分提交成功：隊伍 $teamId 的分數為 $score。<br>";
-            } else {
-                echo "提交失敗：隊伍 $teamId 的分數無法保存。" . mysqli_error($link) . "<br>";
-            }
-        }
-    }
-} else {
-    echo "無效的請求方式。";
-}
-
-// 關閉資料庫連接
-mysqli_close($link);
+echo"成功";
 ?>
