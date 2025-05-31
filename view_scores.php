a424c8bc337970149fe022ce46c7652aa9e0ac77
lobsterXC <a1115514@mail.nuk.edu.tw>
2025-06-01 03:20:12 +0800

    CSS 分離完畢

--- view_scores.php
+++ view_scores.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>高雄大學創意競賽管理系統</title>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="view_scores.css">
 </head>
 <body>
   <header>
